29dd65990c3c4b88054994a5dafe13fb61c8d73c
Isomi <Isomi@Isomi-MacBook-Pro.local>
2016-07-13 05:03:13 -0500

    Added dummy content for portfolio section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,76 @@
+var owlOptions = {
+    // Most important owl features
+    items : 6, //6 items above 1000px browser width
+    itemsDesktop : [1000,5], //5 items between 1000px and 901px
+    itemsDesktopSmall : [900,4], // between 900px and 601px
+    itemsTablet : [600,3], //2 items between 600 and 0
+    itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+
+    //Basic Speeds
+    slideSpeed : 200,
+    paginationSpeed : 800,
+    rewindSpeed : 1000,
+
+    //Autoplay
+    autoPlay : true,
+    stopOnHover : true,
+
+    // Navigation
+    navigation : false,
+    navigationText : ["prev","next"],
+    rewindNav : true,
+    scrollPerPage : false,
+
+    //Pagination
+    pagination : false,
+    paginationNumbers: false,
+
+    // Responsive
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+
+    // CSS Styles
+    baseClass : "owl-carousel",
+    theme : "owl-theme",
+
+    //Lazy load
+    lazyLoad : false,
+    lazyFollow : true,
+    lazyEffect : "fade",
+
+    //Auto height
+    autoHeight : false,
+
+    //JSON
+    jsonPath : false,
+    jsonSuccess : false,
+
+    //Mouse Events
+    dragBeforeAnimFinish : true,
+    mouseDrag : true,
+    touchDrag : true,
+
+    //Transitions
+    transitionStyle : false,
+
+    // Other
+    addClassActive : false,
+
+    //Callbacks
+    beforeUpdate : false,
+    afterUpdate : false,
+    beforeInit: false,
+    afterInit: false,
+    beforeMove: false,
+    afterMove: false,
+    afterAction: false,
+    startDragging : false,
+    afterLazyLoad : false
+};
+
 $(document).ready(function() {
+    // Call fullPage Plugin
     $('#site-main').fullpage({
         //Navigation
         menu: '#main-menu',
@@ -58,4 +130,10 @@ $(document).ready(function() {
         afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
         onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
     });
+
+    // =========================================================================
+    // Call Owl Carousel Plugin
+    $('#web-portfolio').owlCarousel(owlOptions);
+
+    $('#games-portfolio').owlCarousel(owlOptions);
 });

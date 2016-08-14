0e9be6bce29779e95e4893f641af33b3da0f7df9
Isomi <Isomi@Isomi-MacBook-Pro.local>
2016-08-14 17:09:41 -0500

    Mail Controller testing

--- js/mail-controller.js
+++ js/mail-controller.js
@@ -1,9 +1,10 @@
 $('#contact-form').submit(function(event) {
-    event.preventDefault();
     $.ajax({
         url: "https://formspree.io/h.espinola@icloud.com",
         method: "POST",
-        data: {message: "hello!"},
-        dataType: "json"
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
+        }
     });
 });

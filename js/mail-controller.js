f8cc681ecda44a9bec0f6dcc7e80fcaad3b46898
Isomi <Isomi@Isomi-MacBook-Pro.local>
2016-08-17 01:13:17 -0500

    Mail controller sends data using AJAX

--- js/mail-controller.js
+++ js/mail-controller.js
@@ -1,8 +1,20 @@
+// On submit form
 $('#contact-form').submit(function(event) {
+    // Get data from the form
+    var contactName = $('#contact-name').val();
+    var contactEmail = $('#contact-email').val();
+    var contactMessage = $('contact-message').val();
+
+    // Send it using AJAX
     $.ajax({
         url: "https://formspree.io/h.espinola@icloud.com",
         method: "POST",
         dataType: "json",
+        data: {
+            name: contactName,
+            email: contactEmail,
+            message: contactMessage
+        },
         success: function(data) {
             console.log(data);
         }
